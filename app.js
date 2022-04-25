8aecd91221647e38b76c8b2fc5bd7d9450cf5d03
akshay <akshaysharma5432@gmail.com>
2022-04-25 13:29:44 +0530

    make dynamic html using js

--- app.js
+++ app.js
@@ -37,52 +37,6 @@ tabContainer.addEventListener("click", function (e) {
     .querySelector(`.operations__content--${element.dataset.tab}`)
     .classList.add("operations__content--active");
 });
-// Slider component
-const slides = document.querySelectorAll(".slide");
-const btnLeft = document.querySelector(".slider__btn--left");
-const btnRight = document.querySelector(".slider__btn--right");
-let currSlide = 0;
-let maxSlides = slides.length;
-
-const moveSlide = function (slide) {
-  slides.forEach(
-    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%`)
-  );
-};
-// slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
-// in order to provide the desired positions to the slides we will pass 0 to the moveslide function, which will result in 0% 100%, 200%
-moveSlide(0);
-const moveAhead = function () {
-  if (currSlide === maxSlides - 1) {
-    currSlide = 0;
-  } else {
-    currSlide++;
-  }
-  moveSlide(currSlide);
-};
-const moveBehind = function () {
-  if (currSlide === 0) {
-    currSlide = maxSlides - 1;
-  } else {
-    currSlide--;
-  }
-  moveSlide(currSlide);
-};
-
-btnRight.addEventListener("click", moveAhead);
-btnLeft.addEventListener("click", moveBehind);
-// handling arrow keys
-document.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowLeft") {
-    moveBehind();
-  } else if (e.key === "ArrowRight") {
-    moveAhead();
-  }
-});
-//making the dots work
-const createDots = function () {
-  slides.forEach(_, i);
-};
 //implementing page navigation
 const nav = document.querySelector(".main-nav-list");
 const navLinks = document.querySelectorAll(".main-nav-link");
@@ -175,4 +129,147 @@ const expSection = new IntersectionObserver(removeOverflow, {
   root: null,
   threshold: 0.2,
 });
-expSection.observe(exp);
+///Cards component
+const projects = [
+  {
+    title: "The Ipod Project",
+    img: "./assets/ipod.png",
+    list: ["ReactJs", "JSX", "CSS", "JavaScript"],
+    backTitle: "Project Using",
+    backPara: "React",
+    liveLink: "https://ipodbyakshays05.netlify.app/",
+    githubLink: "https://github.com/AkshayS05/Ipod-project-in-react",
+  },
+  {
+    title: "The Pokemon Project",
+    img: "./assets/ipod.png",
+    list: ["ReactJs", "JSX", "Advanced CSS", "JavaScript"],
+    backTitle: "Project Using",
+    backPara: "React",
+    liveLink: "https://pokemonmatch-as05.netlify.app/",
+    githubLink: "https://github.com/AkshayS05/pokemon-match-react-app/",
+  },
+  {
+    title: "Awesome Food Project",
+    img: "./assets/awesomefood.png",
+    list: ["ReactJs", "CSS", "JavaScript", "Web Design"],
+    backTitle: "Project Using",
+    backPara: "HTML, CSS, JS",
+    liveLink: "https://omnifoodbyakshays05.netlify.app/",
+    githubLink: "#",
+  },
+  {
+    title: "Recipe Finder Project",
+    img: "./assets/recipe.png",
+    list: ["ReactJs", "JSX", "CSS", "JavaScript"],
+    backTitle: "Project Using",
+    backPara: "React",
+    liveLink: "https://awesome-recipes-5dd0e.web.app/",
+    githubLink: "https://github.com/AkshayS05/recipeFinder",
+  },
+  {
+    title: "TODO NODE JS APP",
+    img: "./assets/todo.png",
+    list: ["Mongo DB", "Express Js", "EJS", "NodeJs"],
+    backTitle: "Project Using",
+    backPara: "NodeJs",
+    liveLink: "#",
+    githubLink: "https://github.com/AkshayS05/Todo-App",
+  },
+  {
+    title: "DS & ALGO IN JAVA",
+    img: "./assets/java.png",
+    list: ["Sorting Algos", "Searching Algos", "Data Structures", "OOP"],
+    backTitle: "DS & ALGO IN",
+    backPara: "JAVA",
+    liveLink: "#",
+    githubLink: "https://github.com/AkshayS05/Binary_Search",
+  },
+];
+
+const elem = document.querySelector(".slider");
+
+projects.forEach((project, i) => {
+  const html = `
+<div class="slide">
+  <div class="col-${i + 1}">
+    <div class="card">
+      <div class="card__side card__side--front">
+        <div class="card__picture card__picture--${i + 1}">&nbsp;</div>
+
+        <h4 class="card__heading">
+          <span class="card__heading-span card__heading-span--${i + 1}">
+            ${project.title}
+          </span>
+        </h4>
+        <div class="card__details">
+          <ul>
+            <li><i class="fa-solid fa-check"></i>${project.list[0]}</li>
+            <li><i class="fa-solid fa-check"></i>${project.list[1]}</li>
+            <li><i class="fa-solid fa-check"></i>${project.list[2]}</li>
+            <li><i class="fa-solid fa-check"></i>${project.list[3]}</li>
+         
+          </ul>
+        </div>
+      </div>
+      <!-- back -->
+      <div class="card__side card__side--back card__side--back-${i + 1}">
+        <div class="card__cta">
+          <div class="card__text-box">
+            <p class="card__project">${project.backTitle}</p>
+            <p class="card__name">${project.backPara}</p>
+          </div>
+          <a href=${project.liveLink} class= "btn--effect btn--effect--white"
+    target="_blank">View Live</a>
+          <a href=${
+            project.githubLink
+          } class="btn--git btn--git--black" target="_blank"><span><i class="fa-brands fa-github"></i></span>Github</a>
+         
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>`;
+  elem.insertAdjacentHTML("beforeend", html);
+});
+const slides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+let currSlide = 0;
+let maxSlides = slides.length;
+
+const moveSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%`)
+  );
+};
+// slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+// in order to provide the desired positions to the slides we will pass 0 to the moveslide function, which will result in 0% 100%, 200%
+moveSlide(0);
+const moveAhead = function () {
+  if (currSlide === maxSlides - 1) {
+    currSlide = 0;
+  } else {
+    currSlide++;
+  }
+  moveSlide(currSlide);
+};
+const moveBehind = function () {
+  if (currSlide === 0) {
+    currSlide = maxSlides - 1;
+  } else {
+    currSlide--;
+  }
+  moveSlide(currSlide);
+};
+
+btnRight.addEventListener("click", moveAhead);
+btnLeft.addEventListener("click", moveBehind);
+// handling arrow keys
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveBehind();
+  } else if (e.key === "ArrowRight") {
+    moveAhead();
+  }
+});
